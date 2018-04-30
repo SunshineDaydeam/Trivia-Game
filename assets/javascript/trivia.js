638a35678dec10d281be7f6e2cd30c0a1be98799
Sunshine Daydream <alexbruner@live.com>
2018-04-30 01:57:55 -0500

    finally got the Blanking setinverval to reset after clicking next question

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,25 +1,21 @@
-//global variables
-var userGuess=[];                   //user guess
-var correctGuesses=0;               //number of correct guesses
-var wrongGuesses=0;                 //number of incorrect Guesses
-var guessTime=15000;                //time per question
+
 
 //trivia questions
-var q1= {question: "How many years after retiring is a player eligible for the hall of fame?",
+var q1 = {question: "How many years after retiring is a player eligible for the hall of fame?",
     possAnswer1:"3",
     possAnswer2:"5",
     possAnswer3:"6",
     possAnswer4:"10",
     correctAnswer:"5"
     };
-var q2= {question: "How many teams are in the nfl?",
+var q2 = {question: "How many teams are in the nfl?",
     possAnswer1:"28",
     possAnswer2:"30",
     possAnswer3:"32",
     possAnswer4:"36",
     correctAnswer:"32"
     };
-var q3= {question: "Which NFL team has the most Super Bowl Wins?",
+var q3 = {question: "Which NFL team has the most Super Bowl Wins?",
     possAnswer1:"The New England Patriots",
     possAnswer2:"The Pittsburg Steelers",
     possAnswer3:"The Dallas Cowboys",
@@ -61,44 +57,113 @@ var q9 = {question: "Who is the only quarterback in NFL history to have a career
     possAnswer2:"Tom Brady",
     possAnswer3:"Steve Young",
     possAnswer4:"Peyton Manning",
-    correctAnswer:"Aaron Rogers"};
+    correctAnswer:"Aaron Rogers"
+};
 
-//list all questions and answers in an array
-var questionsArray =[q1, q2, q3, q4, q5, q6, q7, q8, q9];
+//global variables
+var userGuess=[];                   //user guess
+var correctGuesses=0;               //number of correct guesses
+var wrongGuesses=0;                 //number of incorrect Guesses
+var guessTime=30;                //time per question
+var prevQuestions=[];               //array of previously answered questions
+var isRunning=true;
+var timer;
 
-var i= Math.floor(Math.random() * Math.floor(questionsArray.length));
+var questionsArray =[q1, q2, q3, q4, q5, q6, q7, q8];
+var randomNumber=null;
+var numberOrder=[];
+//determines a nonrepeating number
+
+
+function questionOrder(){
+    var usedNums=[]
+    for (i=0; i<questionsArray.length; i++){
+        usedNums.push(i);}
+    for (j=0; j<questionsArray.length; j++){
+        randomNumber = usedNums[Math.floor(Math.random() * usedNums.length)]
+        usedNums.splice((usedNums.indexOf(randomNumber)),1 );
+        numberOrder.push(randomNumber);
+    }
+}
+questionOrder();
+
+userQuestion();
 
 function userQuestion(){
+
+    //function variables
+    i = numberOrder[0];
+    isRunning=true;
+    guessTime=30;
+    userGuess=[];
+
+    //start timer
+    timer = setInterval(function(){
+        guessTime--;
+        $("#timer").text(guessTime + " Yay " );
+        console.log(guessTime);
+        }, 1000);
+    
+
+    //display questions and answer choices on screen
     $("#question").text(questionsArray[i].question);
     $("#choice1").text(questionsArray[i].possAnswer1);
     $("#choice2").text(questionsArray[i].possAnswer2);
     $("#choice3").text(questionsArray[i].possAnswer3);
     $("#choice4").text(questionsArray[i].possAnswer4);
-    $("#correctAnswer").text(questionsArray[i].correctAnswer);
-
+    
+    
+    //Click Handlers splice selected answer to userGuess[]
     $("#choice1").click (function(){
-    if(questionsArray[i].possAnswer1 === questionsArray[i].correctAnswer){
-        console.log("Yay")}
-    else{
-        console.log("Nope")}
+        userGuess.splice(0, 1, questionsArray[i].possAnswer1);
+        $("#userGuess").text("You Guessed: " + userGuess);
     });
     $("#choice2").click (function(){
-    if(questionsArray[i].possAnswer2 === questionsArray[i].correctAnswer){
-        console.log("Yay")}
-    else{
-        console.log("Nope")}
-    });;
+        userGuess.splice(0, 1, questionsArray[i].possAnswer2);
+        $("#userGuess").text("You Guessed: " + userGuess);
+    });
     $("#choice3").click (function(){
-    if(questionsArray[i].possAnswer3 === questionsArray[i].correctAnswer){
-        console.log("Yay")}
-    else{
-        console.log("Nope")}
-    });;
+        userGuess.splice(0, 1, questionsArray[i].possAnswer3);
+        $("#userGuess").text("You Guessed: " + userGuess);
+    }); 
     $("#choice4").click (function(){
-        if(questionsArray[i].possAnswer4 === questionsArray[i].correctAnswer){
-        console.log("Yay")}
-        else{
-        console.log("Nope")}
-    });;
-};
-userQuestion();
+        userGuess.splice(0, 1, questionsArray[i].possAnswer4);
+        $("#userGuess").text("You Guessed: " + userGuess);
+    });
+    
+    
+    $("#submit").click(function(){
+        if (isRunning == true){
+            clearInterval(timer);
+            if (userGuess == questionsArray[i].correctAnswer){
+                numberOrder.splice(0, 1);
+                isRunning=false;
+                correctGuesses++;
+                console.log("You are correct. CorrectGuesses: " + correctGuesses);
+            }
+            else if (userGuess != []){
+                numberOrder.splice(0, 1);
+                wrongGuesses++;
+                isRunning=false;
+                console.log("false. wrong guesses: " + wrongGuesses);
+            }
+            else{
+                numberOrder.splice(0, 1);
+                wrongGuesses++;
+                isRunning=false;
+                console.log("false. wrong guesses: " + wrongGuesses);
+            }
+        }
+    });
+
+        $("#next").click(function(){
+            if(isRunning == false){
+                userQuestion();
+            }
+            
+        });
+
+}
+
+
+
